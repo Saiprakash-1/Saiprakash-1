39e8b0c76cf8876845e8b7df3c8fd45746a57702
Saiprakash-1 <132384338+Saiprakash-1@users.noreply.github.com>
2024-07-17 23:14:28 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import {Route, Routes} from "react-router-dom";
+import IndexPage from './Pages/IndexPage';
+import LoginPage from './Pages/LoginPage';
+import Layout from './layout';
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Layout />}>
+      <Route index element={<IndexPage />} />
+      <Route path ='/login' element={<LoginPage />} />
+      </Route>
+    </Routes> 
+  )
+}
+
+export default App
